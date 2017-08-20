c5e75121b0158e5afc9afc89b734445023ae77d3
Andreas Benz <andreasbenz@aiyana4u-mail.de>
2017-08-20 06:05:57 +0200

    Projektdateien hinzufügen.

---
+++ Aiyana_Grid/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aiyana_Grid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
